b91ffdc4fec2ce35308b6bd089a454b72fbfcaa5
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-01-29 14:22:46 -0500

    finished journal entry 2

---
+++ journal.js
@@ -0,0 +1,27 @@
+var journalEntry = [""];
+
+let journalData = {
+    dateOfEntry:"1/15/2019",
+    concepts: "html,css",
+    entry: "covered html and css, had a blast the entire time",
+    mood: "happy"
+}
+let journalData1 = {
+    dateOfEntry:"1/20/2019",
+    concepts: "started Javascript",
+    entry: "Stared learning javascript, we're just getting into it but it could be fun!",
+    mood: "happy"
+}
+let journalData2 = {
+    dateOfEntry:"1/25/2019",
+    concepts: "javascript",
+    entry: "I hate javascript",
+    mood: "sad"
+}
+journalEntry.push(journalData)
+journalEntry.push(journalData1)
+journalEntry.push(journalData2)
+console.log(journalEntry)
+
+
+
