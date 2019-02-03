78e1b324cdb3931a4fd0f5e93de8d94c383b2a0c
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-02-02 19:38:52 -0500

    finished journal 3

--- journal.js
+++ journal.js
@@ -1,4 +1,4 @@
-var journalEntry = [""];
+const journalEntry = [""];
 
 let journalData = {
     dateOfEntry:"1/15/2019",
@@ -23,5 +23,21 @@ journalEntry.push(journalData1)
 journalEntry.push(journalData2)
 console.log(journalEntry)
 
+document.querySelector("#submitButton").addEventListener("click", () =>{
+    const printDate = document.querySelector("#journalDate").value
+    console.log(printDate);
 
+    const printMessage = document.querySelector("#journalEntry").value
+    console.log(printMessage);
 
+    const printConcepts = document.querySelector("#conceptsCovered").value
+    console.log(printConcepts);
+
+    const printMood = document.querySelector("#mood").value
+    console.log(printMood);
+
+    const printAll = `<h1>Date: ${printDate}</h1><h2>Entry: ${printMessage}</h2><h2>Concepts Covered: ${printConcepts}</h2><h2>Mood: ${printMood}</h2>`
+    console.log(printAll)
+
+    document.querySelector("#entryLog").innerHTML = printAll;
+})
